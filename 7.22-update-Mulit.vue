3615cdf6e91a8e7a9cb94ce552340e6485eb17fb
kuminkyu9 <minkugu@naver.com>
2022-07-22 15:19:59 +0900

    update evnt

---
+++ 7.22-update-Mulit.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <input v-model="val" v-on:keyup.enter="setMtVal" placeholder="구구단" class="inputMt">
+    <div>{{val}}단</div>
+
+
+    <div v-if="mtShow" class="container">
+      <ul v-for="(mtDatas, index) in mtTableData" v-bind:key="mtDatas" class="box">
+        <li v-for="mtRight in mtTableData[index].mtRight" v-bind:key="mtRight">{{mtTableData[index].id}} x {{mtRight}} = {{mtTableData[index].id*mtRight}}</li>
+        <div v-on:click="mtDel(index, mtTableData[index].id)" class="delBtn">{{mtTableData[index].id}}단 삭제하기</div>
+
+        <!-- update -->
+        <input v-on:keyup.enter="testUpdate(index)" :class="[`inputMt updateBtn${index}`]" placeholder="입력후 enter update">
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      val: '',
+      mtShow: false,
+      mtTableData: [
+        // {id: i, mtRight: [1,2,3,4,5,6,7,8,9], onOff: true} this.mtTableData.push({id: i, mtRight: [1,2,3,4,5,6,7,8,9]})
+      ],
+    }
+  },
+  methods: {
+    setMtVal() {
+      this.mtTableData = []; //arr reset
+
+      this.mtShow = true; //mt show 
+
+      for(var i=0; i<this.val; i++) { //mtTableData push
+        this.mtTableData.push({id: i+1, mtRight: [1,2,3,4,5,6,7,8,9], onOff: true});  //id mtLeftVal, onOff v-if boolean
+      }
+    },
+    mtDel(id, mtIndex) { //del btn
+      this.mtTableData.splice(id, 1);
+      alert(`${mtIndex}단이 삭제되었습니다.`)
+    },
+
+    // update test
+    testUpdate(index) {
+        console.log(this.mtTableData[index].id);
+        console.log(Number(document.querySelector(`.updateBtn${index}`).value))
+        this.mtTableData[index].id = Number(document.querySelector(`.updateBtn${index}`).value);
+    }
+  }
+}
+</script>
+
+<style>
+  li {
+    list-style: none;
+  }
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  .container .box {
+    transition: .2s;
+    padding: 20px;
+    background: #fff;
+    border-radius: 0.5rem;
+    box-shadow: 0 4px 25px 0 rgb(0 0 0 / 10%);
+  }
+  .container .box:hover {
+    transition: .2s;
+    transform: translateY(-5%);
+  }
+  .inputMt {
+    padding: 5px;
+    border: 1px solid gray;
+    border-radius: 7px;
+    box-shadow: rgb(0 0 0 / 10%) 0px 5px 10px 0px;
+    outline: none;
+    margin: 15px 0;
+  }
+  .inputMt:focus {
+    animation: vibration .1s 2;
+  }
+  .delBtn {
+    transition: .3s;
+    margin-top: 15px;
+    padding: 5px;
+    border-radius: 7px;
+    box-shadow: rgb(0 0 0 / 10%) 0px 5px 10px 0px;
+    cursor: pointer;
+  }
+  .delBtn:hover {
+    transition: .3s;
+    background: rgba(255, 6, 6, 0.493);
+  } 
+
+  @keyframes vibration {
+    from {
+      transform: rotate(1deg);
+    }
+    to {
+      transform: rotate(-1deg);
+    }
+  }
+</style>
